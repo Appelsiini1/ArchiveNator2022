f31be1c28727361471ec9302c86080c604681a68
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2022-06-28 00:42:47 +0300

    Moved initialization functions to their own file

---
+++ initialization.py
@@ -0,0 +1,34 @@
+import logging
+from PySimpleGUI import UserSettings
+from constants import ENVPATH, VERSION
+from os import path, mkdir
+
+def load_configs():
+    """Load program configurations"""
+    settings = UserSettings(filename=path.join(ENVPATH, "settings.json"))
+    if bool(settings):
+        logging.info("No settings found, loading defaults.")
+        settings["theme"] = "Reddit"
+        settings["ButtonColour"] = "Red"
+        settings["defaultDBPath"] = ENVPATH
+
+    logging.info("Settings loaded.")
+    return settings
+
+
+def init():
+    """Program initiliazation"""
+
+    logname = path.join(ENVPATH, "log.txt")
+    if path.exists(ENVPATH) is False:
+        mkdir(ENVPATH)
+    logging.basicConfig(
+        filename=logname,
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s - %(message)s",
+        datefmt="%d/%m/%Y %H:%M:%S",
+    )
+
+    logging.info(  # pylint: disable=logging-fstring-interpolation
+        f"ArchiveNator2022 v{VERSION}"
+    )
--- main.py
+++ main.py
@@ -1,41 +1,12 @@
 """ArchiveNator2022 is a Python program to index physical documents to a database manually."""
-import logging
-import os
 import sys
 import PySimpleGUI as sg
-from constants import VERSION, ENVPATH, MENU_DEF
-import archive_window
 
 
-def load_configs():
-    """Load program configurations"""
-    settings = sg.UserSettings(filename=os.path.join(ENVPATH, "settings.json"))
-    if bool(settings):
-        logging.info("No settings found, loading defaults.")
-        settings["theme"] = "Reddit"
-        settings["ButtonColour"] = "Red"
-        settings["defaultDBPath"] = ENVPATH
+from constants import MENU_DEF
+import initialization
+import archive_main_window
 
-    logging.info("Settings loaded.")
-    return settings
-
-
-def init():
-    """Program initiliazation"""
-
-    logname = os.path.join(ENVPATH, "log.txt")
-    if os.path.exists(ENVPATH) is False:
-        os.mkdir(ENVPATH)
-    logging.basicConfig(
-        filename=logname,
-        level=logging.INFO,
-        format="%(asctime)s %(levelname)s - %(message)s",
-        datefmt="%d/%m/%Y %H:%M:%S",
-    )
-
-    logging.info(  # pylint: disable=logging-fstring-interpolation
-        f"ArchiveNator2022 v{VERSION}"
-    )
 
 def define_main_window():
     '''Define main window'''
@@ -85,8 +56,8 @@ def define_main_window():
 
 def main():
     """Main event loop and startup"""
-    init()
-    settings = load_configs()
+    initialization.init()
+    settings = initialization.load_configs()
 
     sg.theme(settings["theme"])
     sg.theme_button_color(color="Red")
@@ -103,7 +74,7 @@ def main():
             if file == "":
                 continue
             main_window.Hide()
-            archive_window.arc_window(file)
+            archive_main_window.arc_window(file)
             main_window.UnHide()
         elif event == "invisible2":
             file = values["invisible2"]
@@ -112,7 +83,7 @@ def main():
             elif file.split("/")[-1].split(".")[-1] != "db":
                 file += ".db"
             main_window.Hide()
-            archive_window.create_new(file)
+            archive_main_window.create_new(file)
             main_window.UnHide()
         elif event == "Asetukset":
             sg.PopupOK("Asetukset", title="Testi")
