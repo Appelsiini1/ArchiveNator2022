1225a214ec62532f24929d1930b1fa5655410f23
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2022-06-28 00:44:07 +0300

    Move DB info function to the proper file

--- db_func.py
+++ db_func.py
@@ -1,10 +1,26 @@
 '''Database functions'''
 import sqlite3
-import PySimpleGUI as sg
+import string
 
-def add_item(c, item):
-    pass
+def get_db_info(file:string):
+    """Returns a list with the database information"""
+    with sqlite3.connect(file) as conn:
+        c = conn.cursor()
+        c.execute("SELECT db_name FROM DB_info")
+        db_name = c.fetchone()[0]
 
-def del_item(c, item):
-    pass
+        c.execute("SELECT db_desc FROM DB_info")
+        db_desc = c.fetchone()[0]
+        if db_desc == "":
+            db_desc = "(Tyhjä)"
+        # desc_newlines = db_desc.count("\n")
 
+        c.execute("SELECT db_table_count, db_tables FROM DB_info")
+        db_tables_raw = c.fetchall()[0]
+        db_table_count = db_tables_raw[0]
+        db_tables = [db_tables_raw[1]]
+        if db_table_count == 0:
+            db_tables = ["(Uusi taulu)"]
+        else:
+            db_tables = ["(Uusi taulu)"] + db_tables[0].split(";")
+    return [db_name, db_desc, db_tables]
